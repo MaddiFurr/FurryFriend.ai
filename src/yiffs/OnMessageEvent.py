6a1cb9f0939247f4366d65aa220823c6787b5899
Maddi <her@maddi.wtf>
2024-01-15 12:10:59 -0600

    Changed to TwittPR instead of VXTwitter

--- src/yiffs/OnMessageEvent.py
+++ src/yiffs/OnMessageEvent.py
@@ -7,13 +7,13 @@ async def on_message(message):
         return
     avatar = str(bot.get_user(message.author.id).avatar)
     print(avatar)
-    ## Replace Twitter Links with VXTwitter Links
+    ## Replace Twitter Links with TwittPR Links
     if "https://twitter.com" in message.content:
-        await send_webhook_message(message.guild, message.channel, message.author, avatar, message.content.replace("https://twitter.com/","https://vxtwitter.com/"))
+        await send_webhook_message(message.guild, message.channel, message.author, avatar, message.content.replace("https://twitter.com/","https://twittpr.com/"))
         await message.delete()
-        print("Replaced {} with VXTwitter Link".format(message.content))
+        print("Replaced {} with TwittPR Link".format(message.content))
     if "https://x.com/" in message.content:
-        await send_webhook_message(message.guild, message.channel, message.author, avatar, message.content.replace("https://x.com/","https://vxtwitter.com/"))
+        await send_webhook_message(message.guild, message.channel, message.author, avatar, message.content.replace("https://x.com/","https://twittpr.com/"))
         await message.delete()
-        print("Replaced {} with VXTwitter Link".format(message.content))
+        print("Replaced {} with TwittPR Link".format(message.content))
     await bot.process_commands(message)
